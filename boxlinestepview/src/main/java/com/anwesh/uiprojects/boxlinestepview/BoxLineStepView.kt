2eb2a7d694a50266c00d500dfe40306b13e7094d
Anwesh43 <anweshthecool0@gmail.com>
2018-11-09 10:24:13 +0530

    adding Float extension methods

--- boxlinestepview/src/main/java/com/anwesh/uiprojects/boxlinestepview/BoxLineStepView.kt
+++ boxlinestepview/src/main/java/com/anwesh/uiprojects/boxlinestepview/BoxLineStepView.kt
@@ -15,7 +15,19 @@ import android.content.Context
 
 val nodes : Int = 5
 
-val lines : Int = 3
+val INNER_LINES : Int = 3
+
+val OUTER_LINES : Int = 4
+
+val scGap : Float = 0.05f
+
+fun Int.getInverse() : Float = 1f/this
+
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.getInverse(), Math.max(this - i * n.getInverse(), 0f))
+
+fun Float.scaleFactor() : Float = Math.floor(this / 0.5).toFloat()
+
+fun Float.updateScale(dir : Float) : Float = dir * scGap * (scaleFactor() / OUTER_LINES + (1f - scaleFactor()) / INNER_LINES)
 
 class BoxLineStepView(ctx : Context) : View(ctx) {
 
