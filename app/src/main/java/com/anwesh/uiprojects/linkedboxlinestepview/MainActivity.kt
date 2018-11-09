d5fd2d75789eb44b324f6910efa6752423c9c859
Anwesh43 <anweshthecool0@gmail.com>
2018-11-09 23:40:14 +0530

    adding OnAnimationListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedboxlinestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedboxlinestepview/MainActivity.kt
@@ -3,14 +3,20 @@ package com.anwesh.uiprojects.linkedboxlinestepview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.boxlinestepview.BoxLineStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BoxLineStepView.create(this)
+        val view : BoxLineStepView = BoxLineStepView.create(this)
         fullScreen()
+        view.addOnAnimationListener({createToast("animation is complete $it")}, {createToast("animation is reset $it")})
+    }
+
+    fun createToast(txt : String) {
+        Toast.makeText(this, txt, Toast.LENGTH_SHORT).show()
     }
 }
 
