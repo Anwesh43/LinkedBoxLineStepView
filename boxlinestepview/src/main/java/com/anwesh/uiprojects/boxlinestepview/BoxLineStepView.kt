fa97724ad8c6a1608c28730f295e698bb7d3b315
Anwesh43 <anweshthecool0@gmail.com>
2018-11-09 11:06:57 +0530

    adding BoxLineStep class

--- boxlinestepview/src/main/java/com/anwesh/uiprojects/boxlinestepview/BoxLineStepView.kt
+++ boxlinestepview/src/main/java/com/anwesh/uiprojects/boxlinestepview/BoxLineStepView.kt
@@ -171,4 +171,28 @@ class BoxLineStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BoxLineStep(var i : Int) {
+
+        private var curr : BLSNode = BLSNode(0)
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
