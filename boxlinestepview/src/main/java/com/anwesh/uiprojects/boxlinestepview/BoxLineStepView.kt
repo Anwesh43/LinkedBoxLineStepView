3cebb45939844e46c8195f7f9de3943f6a8994b2
Anwesh43 <anweshthecool0@gmail.com>
2018-11-09 10:13:57 +0530

    adding BoxLineStepView class

--- boxlinestepview/src/main/java/com/anwesh/uiprojects/boxlinestepview/BoxLineStepView.kt
+++ boxlinestepview/src/main/java/com/anwesh/uiprojects/boxlinestepview/BoxLineStepView.kt
@@ -11,8 +11,24 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.RectF
 import android.app.Activity
+import android.content.Context
 
 val nodes : Int = 5
 
 val lines : Int = 3
 
+class BoxLineStepView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
