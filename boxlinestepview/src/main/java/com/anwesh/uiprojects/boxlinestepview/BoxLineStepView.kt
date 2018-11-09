92be0af9797a51a99472bdf758c2e5a106f2d5b5
Anwesh43 <anweshthecool0@gmail.com>
2018-11-09 10:10:30 +0530

    importing libraries and defining constants

--- boxlinestepview/src/main/java/com/anwesh/uiprojects/boxlinestepview/BoxLineStepView.kt
+++ boxlinestepview/src/main/java/com/anwesh/uiprojects/boxlinestepview/BoxLineStepView.kt
@@ -4,3 +4,15 @@ package com.anwesh.uiprojects.boxlinestepview
  * Created by anweshmishra on 09/11/18.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+
+val nodes : Int = 5
+
+val lines : Int = 3
+
